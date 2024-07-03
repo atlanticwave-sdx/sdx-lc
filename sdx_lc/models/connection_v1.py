51f3a5349db2a7d3ed41d54f8d5466116b4bea10
Yufeng Xin <yxin@renci.org>
2024-07-03 10:58:47 -0400

    import sdx-lc.model

--- sdx_lc/models/connection_v1.py
+++ sdx_lc/models/connection_v1.py
@@ -5,10 +5,10 @@ from __future__ import absolute_import
 from datetime import date, datetime  # noqa: F401
 from typing import Dict, List  # noqa: F401
 
-from sdx_datamodel import util
-from sdx_datamodel.models.base_model_ import Model
-from sdx_datamodel.models.link import Link  # noqa: F401,E501
-from sdx_datamodel.models.port import Port  # noqa: F401,E501
+from sdx_lc import util
+from sdx_lc.models.base_model_ import Model
+from sdx_lc.models.link import Link  # noqa: F401,E501
+from sdx_lc.models.port import Port  # noqa: F401,E501
 
 
 class Connection(Model):
