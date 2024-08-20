202d03f9a4de21f73366be1da64ce6b7668c4679
Cong Wang <cwang@renci.org>
2024-08-20 20:16:19 +0000

    Format

--- sdx_lc/jobs/pull_topo_changes.py
+++ sdx_lc/jobs/pull_topo_changes.py
@@ -18,7 +18,7 @@ from utils.db_utils import DbUtils
 OXP_PULL_URL = os.environ.get("OXP_PULL_URL")
 OXP_PULL_INTERVAL = os.environ.get("OXP_PULL_INTERVAL")
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
+
 
 def main():
     db_instance = DbUtils()
@@ -72,7 +72,7 @@ def process_domain_controller_topo(db_instance):
         except KeyError:
             logger.debug("Error getting topo version")
             continue
-        
+
         if latest_topology_exists and latest_topo_version == pulled_topo_version:
             time.sleep(int(OXP_PULL_INTERVAL))
             continue
